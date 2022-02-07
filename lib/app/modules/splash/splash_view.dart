0bccaa1977d18de7486ad126738906e824ded7d8
Ricardo Emerson <ricardo_emerson@yahoo.com.br>
2022-02-06 23:14:21 -0300

    Adicionado SystemUiOverlayStyle customizado para splash_view.

--- lib/app/modules/splash/splash_view.dart
+++ lib/app/modules/splash/splash_view.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
-import '../../core/widgets/app_bar_header.dart';
+import '../../core/theme/theme.dart';
 import '../../core/widgets/button.dart';
 import 'splash_controller.dart';
 
@@ -9,16 +10,46 @@ class SplashView extends GetView<SplashController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBarHeader(
+      appBar: AppBar(
+        backgroundColor: backgroundDarkColor,
         elevation: 0,
+        systemOverlayStyle: const SystemUiOverlayStyle(
+          statusBarColor: backgroundDarkColor,
+          statusBarIconBrightness: Brightness.light,
+          statusBarBrightness: Brightness.dark,
+        ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Center(
-          child: Button(
-            label: 'Acessar',
-            onPressed: () {},
-          ),
+      body: Container(
+        color: backgroundDarkColor,
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: SizedBox(
+                width: context.width,
+                child: Image.asset(
+                  'assets/images/lanche.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Center(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: context.heightTransformer(reducedBy: 90),
+                  ),
+                  Image.asset('assets/images/logo.png'),
+                  const SizedBox(height: 60),
+                  Button(
+                    label: 'ACESSAR',
+                    width: context.widthTransformer(reducedBy: 40),
+                    onPressed: () => Get.toNamed('/auth/login'),
+                  )
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
